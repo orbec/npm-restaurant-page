1517b39fac101ce1543e66841190d29a0eded1ad
Jairo Lopez <18562739+orbec@users.noreply.github.com>
2024-09-17 17:39:54 -0500

    modified

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-    mode: "production",
+    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
